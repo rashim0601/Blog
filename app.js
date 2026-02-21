c8863709366a86c5a7f04e50269690abcd45b0e1
Rashim Malhotra <154965493+rashim0601@users.noreply.github.com>
2026-02-21 11:28:50 +0530

    added necessary files

--- app.js
+++ app.js
@@ -1,70 +1,163 @@
-const express = require('express');
-const ejs = require('ejs');
-const path = require('path');
-const bodyParser = require("body-parser");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const currentTime = require(__dirname + '/getDateString')
+const PORT = process.env.PORT || 3000
 
+const homeContent =
+	"Chuck Norris is so fast, he can run around the world and punch himself in the back of the head If you spell Chuck Norris in Scrabble, you win. Forever If you ask Chuck Norris what time it is, he always says, 'Two seconds 'til.' After you ask, 'Two seconds 'til what?' he roundhouse kicks you in the face If you ask Chuck Norris what time it is, he always says, 'Two seconds 'til.' After you ask, 'Two seconds 'til what?' he roundhouse kicks you in the face, Chuck Norris can lead a horse to water AND make it drink Contrary to popular belief, Chuck Norris, not the box jellyfish of northern Australia, is the most venomous creature on earth. Chuck Norris counted to infinity - twice, Chuck Norris has two speeds. Walk, and Kill, Chuck Norris was refused a part in the movie SAW because the traps would have taken him 1 second to escape and roundhouse kick Jiggysaw in the face. When Chuck Norris sends in his taxes, he sends blank forms and includes only a picture of himself, crouched and ready to attack. Chuck Norris has not had to pay taxes, ever Outer space exists because it's afraid to be on the same planet with Chuck Norris The Great Wall of China was originally created to keep Chuck Norris out. It failed miserably. Chuck Norris is currently suing NBC, claiming Law and Order are trademarked names for his left and right legs. When Superman takes a day off, he calls Chuck Norris to fill in for him."
 
-const homeStartingContent = "Your personal space to capture thoughts, ideas, and moments—one day at a time. Whether you're documenting your daily experiences, brainstorming creative ideas, or simply expressing yourself, Daily Journal provides a simple and intuitive platform to write, reflect, and grow."
-const aboutContent = "Welcome to Daily Journal, your personal space to capture thoughts, ideas, and experiences—one day at a time. Whether you're reflecting on daily moments, setting goals, or expressing creativity, this platform is designed to help you write freely and stay organized.";
-const aboutMission = "At Daily Journal, we believe that writing is a powerful tool for self-expression, reflection, and growth. Our goal is to provide a simple, distraction-free, and intuitive platform where you can document your thoughts, revisit past entries, and create a habit of journaling that enriches your daily life.";
-const contactContent = "Have questions, feedback, or suggestions? ";
-const nxtLine = "We'd love to hear from you! Whether you need support or just want to share your thoughts, feel free to reach out." 
+const aboutContent =
+	'Pudding bear claw halvah pastry cake gummi bears. Cheesecake candy canes sesame snaps halvah chocolate I love chupa chups. Jelly-o toffee soufflé marzipan pie pastry brownie. Carrot cake donut icing croissant brownie I love. Jelly icing chocolate bar pudding topping sesame snaps. Pastry biscuit powder wafer lollipop powder. Soufflé cake jelly beans candy cake. Pudding pie bonbon candy canes topping bear claw jelly-o I love. Chocolate bar chupa chups dragée donut macaroon. Powder marshmallow dessert jujubes fruitcake. Jujubes icing candy canes gingerbread lollipop cake pastry. Shortbread pudding chocolate cake I love I love shortbread.'
 
-app.set('view engine', 'ejs');
+const contactContent =
+	'Cake ice cream brownie powder lollipop sweet roll. Biscuit tootsie roll candy canes icing chupa chups. Liquorice jelly-o liquorice chocolate gummies. I love candy canes caramels lollipop apple pie. Candy I love carrot cake tart cupcake. Sugar plum jelly beans sugar plum jelly beans caramels. Bear claw powder danish jelly muffin shortbread lemon drops biscuit shortbread. Carrot cake donut chupa chups I love dessert oat cake gummi bears. Carrot cake pie croissant fruitcake cupcake pudding lollipop caramels. Candy canes powder bonbon jelly-o chocolate bar gingerbread pie. Cheesecake carrot cake pastry cheesecake chocolate cake lemon drops oat cake. Bear claw candy toffee pudding chocolate I love. Pastry I love danish sweet roll liquorice donut jelly-o toffee.'
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+const composeContent =
+	'Write your blog post here, click publish when you ready to publish.'
+const day = currentTime()
 
-let posts = [];
+//set up app
+const app = express()
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
-app.get("/about" , (req, res)=>{
-    res.render("about", {content: aboutContent, mission: aboutMission});
-});
+//set up mongoose
+//create MongoDB database
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI)
+		console.log(`MongoDB Connected: ${conn.connection.host}`)
+	} catch (error) {
+		console.log(error)
+		process.exit(1)
+	}
+}
 
-app.get("/contact" , (req, res)=>{
-    res.render("contact", {content: contactContent,
-        next: nxtLine
-    });
-});
+//create a SCHEMA that sets out the fields each document will have and their datatypes
+const blogsSchema = new mongoose.Schema({
+	blogTitle: {
+		type: String,
+		required: [true, 'No name specified!'],
+	},
+	blogBody: {
+		type: String,
+		required: [true, 'No body specified!'],
+	},
+	blogCreated: {
+		type: String,
+		required: [true, 'Time not specified!'],
+	},
+})
 
-app.get("/compose" , (req, res)=>{
-    res.render("compose");
-});
+//create model from schema
+const Blog = new mongoose.model('Blog', blogsSchema)
+//default posts
+const firstPost = new Blog({
+	blogTitle: 'Cupcakes',
+	blogBody:
+		'Cupcake ipsum dolor sit amet candy canes apple pie oat cake gummies. I love dessert muffin oat cake pudding I love. Cotton candy wafer cookie candy canes lemon drops pie marshmallow gingerbread. Sesame snaps chupa chups bonbon I love danish bear claw. Bonbon powder jelly beans jelly gummi bears jelly beans. Chocolate cake jujubes I love dragée jelly shortbread liquorice. Gummies I love jelly-o fruitcake lollipop. Sesame snaps muffin chocolate bar chocolate bar sugar plum soufflé. Tart tootsie roll topping pie lollipop powder topping icing. I love soufflé brownie chocolate jelly-o dessert apple pie cookie. Halvah cake chocolate jelly sweet sugar plum bonbon donut. Croissant candy sweet roll gingerbread tart cake. Marshmallow jelly chupa chups donut I love. Pastry cotton candy tiramisu muffin I love bonbon sweet tiramisu.',
+	blogCreated: day,
+})
+const secondPost = new Blog({
+	blogTitle: 'Muffins',
+	blogBody:
+		'I love apple pie dragée powder cupcake donut lollipop. Croissant gummi bears chocolate bar I love I love cheesecake lollipop tart. I love tootsie roll toffee marzipan lemon drops cake muffin. I love lollipop powder croissant tiramisu. Sweet roll cheesecake jelly beans sweet gummi bears ice cream marzipan. Biscuit jelly beans I love I love tart gummies macaroon chocolate cake. Toffee candy canes toffee I love cupcake gingerbread carrot cake gummi bears halvah. Dragée oat cake biscuit fruitcake gummi bears pudding marshmallow I love. Powder wafer marshmallow dragée liquorice powder pudding bonbon lollipop. Carrot cake candy I love cookie candy. Candy canes I love sesame snaps muffin candy canes apple pie I love chocolate bar. Dessert marzipan bonbon carrot cake chocolate cake apple pie danish jelly beans. Cookie I love cake sweet roll toffee I love caramels carrot cake croissant. Pastry gummies cake cookie chupa chups. Carrot cake tootsie roll cheesecake jelly wafer halvah bonbon icing pudding. Lollipop gingerbread fruitcake ice cream icing powder. Pie oat cake caramels marzipan halvah gummi bears apple pie lollipop gingerbread. Donut I love powder chocolate bar I love chocolate sweet roll. Muffin sesame snaps I love chupa chups cake bonbon. Gingerbread soufflé donut I love lemon drops I love lemon drops. I love gummi bears muffin lollipop gingerbread icing carrot cake. I love I love lollipop fruitcake cotton candy macaroon bonbon donut. Gingerbread gummi bears tart sugar plum cookie jelly-o. I love halvah pudding I love biscuit jelly beans jujubes sweet roll. Chocolate bar sweet bear claw I love caramels bonbon icing. Danish I love brownie wafer donut oat cake topping chocolate lemon drops.',
+	blogCreated: day,
+})
+const blogsArr = [firstPost, secondPost]
 
-app.post("/compose", (req,res) =>{
-    const post = {
-        title: req.body.titleBody,
-        content: req.body.postBody
-    };
-    posts.push(post);
-    
-    res.redirect("/");
-});
+//get requests
+app.get('/', (req, res) => {
+	if (res.statusCode === 200) {
+		//get all tasks from DB collection and put it on home page
+		Blog.find()
+			.then(function (blogs) {
+				if (blogs.length === 0) {
+					// Function call
+					Blog.insertMany(blogsArr)
+						.then(function () {
+							console.log('Data inserted') // Success
+							res.redirect('/')
+						})
+						.catch(function (error) {
+							console.log(error) // Failure
+						})
+				} else {
+					res.render('home', {
+						startingContent: homeContent,
+						blogs: blogs,
+						mainTitle: 'Home',
+					})
+				}
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+	} else {
+		console.log('Not getting DB blogs')
+	}
+})
 
-app.get("/posts/:postName", (req, res)=>{
-    const requestedTitle = req.params.postName.toLowerCase();
+app.get('/about', (req, res) => {
+	res.render('about', { content: aboutContent, mainTitle: 'About' })
+})
 
-    posts.forEach((post)=>{
-        const storedTitle = post.title.toLowerCase();
+app.get('/contact', (req, res) => {
+	res.render('contact', { content: contactContent, mainTitle: 'Contact' })
+})
 
-        if(storedTitle === requestedTitle){
-            res.render("post.ejs", { 
-                title: post.title,
-                content: post.content});
-        }
-    });
-    
-});
+app.get('/compose', (req, res) => {
+	res.render('compose', { content: composeContent, mainTitle: 'Compose' })
+})
 
-app.get("/", (req, res)=>{
-    res.render("home.ejs" , {
-        homeStartingContent,
-        posts: posts
-    });
-});
+app.get('/:singleBlog', (req, res) => {
+	let path = req.params.singleBlog
+	//find blog that matches search param
+	Blog.findOne({ _id: path })
+		.then(foundBlog => {
+			if (!foundBlog) {
+				console.log('No blog found. Blog=' + foundBlog)
+			} else {
+				//show existing blog
+				res.render('partials/singlePost', {
+					blogTitle: foundBlog.blogTitle,
+					blogBody: foundBlog.blogBody,
+					blogCreated: foundBlog.blogCreated,
+				})
+			}
+		})
+		.catch(err => {
+			console.log(err)
+		})
+})
 
+//post request
+app.post('/compose', (req, res) => {
+	if (res.statusCode === 200) {
+		const newBlogTitle = req.body.blogTitle
+		const newBlogBody = req.body.blogText
+		const newBlogTime = currentTime()
 
-app.listen(3000, ()=>{
-    console.log("Server started on port 3000");
-});
+		let newPost = new Blog({
+			blogTitle: newBlogTitle,
+			blogBody: newBlogBody,
+			blogCreated: newBlogTime,
+		})
+
+		newPost.save()
+
+		res.redirect('/')
+	} else {
+		console.log('something is wrong!')
+	}
+})
+
+//Connect to the database before listening
+connectDB().then(() => {
+	app.listen(PORT, () => {
+		console.log('listening for requests')
+	})
+})
